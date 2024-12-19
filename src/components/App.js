41b515ee05692e56585ee10ad52a03287042c268
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2024-12-19 21:27:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
+import Landing from './Landing'
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Landing />
     </div>
   )
 }
